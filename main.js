07a6166b0f85b54ce60c786d1ff62d9e08a9a9cb
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2024-02-06 19:33:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const paragraphe = document.querySelector("p:nth-child(3)");
+
+const parent = document.querySelector("body");
+
+parent.removeChild(paragraphe);
+
+console.log(paragraphe);
+console.log(parent);
